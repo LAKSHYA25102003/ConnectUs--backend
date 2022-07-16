810ca1bf89df97e99a7c6f2ebad0eaa3497bc6a8
LAKSHYA <llakshya63@gmail.com>
2022-07-16 16:35:13 +0530

    some images are deleted

--- config/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-mongoose.connect(process.env.MONGO_URL)
-const db=mongoose.connection;
-db.on("error",console.error.bind("Connection error"));
-db.once('open',function(){
-    console.log("Connection is made with DB very successfully");
-});
