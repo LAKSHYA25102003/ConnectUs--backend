5f5714b4d663f19b487e460c6212687cea55c770
LAKSHYA <llakshya63@gmail.com>
2023-02-21 20:14:12 +0530

    backend is done with almost all features

---
+++ models/user.js
@@ -0,0 +1,63 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        min:3
+    },
+    email:{
+        type:String,
+        required:true,
+        min:3,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        min:6,
+    },
+    followers:{
+        type:Array,
+        default:[]
+    },
+    following:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    desc:{
+        type:String
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    city:{
+        type:String
+    },
+    from:{
+        type:String
+    },
+    relationship:{
+        type:String,
+    },
+    resetPassword:{
+        type:Boolean,
+        default:false
+    }
+},{
+    timestamps:true
+})
+
+
+const User=mongoose.model("User",userSchema);
+
+module.exports= User;
